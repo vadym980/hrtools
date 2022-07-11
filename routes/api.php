4587d79de466d13801b715f530a313e6e0a2b29e
vadym980 <vadim.gmyrya@ukrainian-it.family>
2022-07-11 18:43:39 +0300

    Registr and auth

--- routes/api.php
+++ routes/api.php
@@ -18,10 +18,10 @@ use App\Http\Controllers\Api\StatusController;
 
 Route::prefix('v1')->group(function () {
 
-    //Route::group(['prefix' => 'auth'], function () {
+    Route::group(['prefix' => 'auth'], function () {
         Route::post('/register', [AuthController::class, 'register']);
         Route::post('/login', [AuthController::class, 'login']);
-    //});
+    });
 
     Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
         return $request->user();
